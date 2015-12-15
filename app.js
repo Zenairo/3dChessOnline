611ac0f5c703a6c3f450dd30957c0fda024641d6
Zenairo <peterlomason@gmail.com>
2015-12-14 19:44:31 -0500

    package updates

--- app.js
+++ app.js
@@ -84,8 +84,8 @@ io.on('connection', function(socket){
 
 });
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 4545;
-var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || 8080;
+var ip_address = process.env.IP_ADDRESS || '127.0.0.1';
 
 http.listen(port, ip_address, function(){
     console.log( "Listening on " + ip_address + ", server_port " + port );
